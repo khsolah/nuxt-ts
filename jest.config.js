a44ea49bc8ff95fccc95ca2ab845b67f043ecdbf
khsolah <khsolah@gmail.com>
2021-06-17 03:29:05 +0800

    build(config): init

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  setupFilesAfterEnv: ['./test/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/plugins/**/*.ts',
+    '<rootDir>/store/**/*.ts'
+  ]
+}
