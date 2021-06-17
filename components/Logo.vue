abc5e477ebf162d36cc4392d69cf545faa8e88f0
khsolah <khsolah@gmail.com>
2021-06-17 15:46:20 +0800

    feat(home page): work with windicss

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    class="NuxtLogo"
+    class="NuxtLogo block"
     width="245"
     height="180"
     viewBox="0 0 452 342"
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="container">
+  <div
+    class="
+      container
+      absolute
+      top-0
+      left-0
+      right-0
+      bottom-0
+      flex
+      justify-center
+      items-center
+    "
+  >
     <div>
       <Logo />
-      <h1 class="title">nuxt-ts</h1>
+      <h1 class="title text-center text-size-[40px] leading-normal">NUXT.TS</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -30,36 +42,3 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
